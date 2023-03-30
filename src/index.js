adeb7c096e936f5851f10ca2bd9aa94678c83608
Anass FERRAK <an.ferrak@gmail.com>
2023-03-30 10:42:48 +0200

    feat : implement AppLayout & inject store in the app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
+
+import { store, persistor } from "./store";
 import App from "./App";
 
 import "./index.css";
@@ -12,7 +15,11 @@ const root = createRoot(container);
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<App />
+			<BrowserRouter>
+				<PersistGate persistor={persistor}>
+					<App />
+				</PersistGate>
+			</BrowserRouter>
 		</Provider>
 	</React.StrictMode>
 );
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,5 +1,37 @@
+import React from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Layout, Badge, theme } from "antd";
+
+const { Header, Content, Footer } = Layout;
+
 const AppLayout = () => {
-	return <p> AppLayout</p>;
+	const navigate = useNavigate();
+	const {
+		token: { colorBgContainer }
+	} = theme.useToken();
+	const { cart } = useSelector(state => state.cart);
+
+	const quantiteTotale = cart.reduce((acc, item) => acc + item.quantity, 0);
+
+	return (
+		<Layout>
+			<Layout className="site-layout">
+				<Header style={{ padding: 20, position: "sticky", background: colorBgContainer, top: 0, zIndex: 1, width: "100%", display: "flex" }}>
+					<Badge count={quantiteTotale}>
+						<ShoppingCartOutlined style={{ fontSize: "35px" }} onClick={() => navigate("cart")} />
+					</Badge>
+				</Header>
+				<Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+					<Outlet />
+				</Content>
+				<Footer style={{ textAlign: "center", bottom: 0, zIndex: 1, width: "100%", background: "white", position: "fixed" }}>
+					Dipeeo Technical Test ©2023 Created by Anass FERRAK
+				</Footer>
+			</Layout>
+		</Layout>
+	);
 };
 
 export default AppLayout;
